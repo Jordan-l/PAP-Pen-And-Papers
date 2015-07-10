92facfd486a0d3c26133009f821f1ee61f1e602f
Jordan-l <jordan-l@freenet.de>
2015-07-10 15:38:53 +0200

    add second activity

--- AndroidApp/.gitattributes/app/src/main/java/com/development/jordan/pap/PAPActivity.java
+++ AndroidApp/.gitattributes/app/src/main/java/com/development/jordan/pap/PAPActivity.java
@@ -1,7 +1,11 @@
 package com.development.jordan.pap;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
 
 public class PAPActivity extends Activity {
@@ -11,4 +15,13 @@ public class PAPActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pap);
     }
+
+    Button buttonNSpiel = (Button) findViewById(R.id.BNeuesSpiel);
+    OnClickListener buttonNeuesSpiel = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent in = new Intent(PAPActivity.this, generalOptions.class);
+            startActivity(in);
+        }
+    };
 }
---
+++ AndroidApp/.gitattributes/app/src/main/java/com/development/jordan/pap/generalOptions.java
@@ -0,0 +1,15 @@
+package com.development.jordan.pap;
+
+/**
+ * Created by Jordan on 10.07.2015.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class generalOptions  extends Activity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gerneral_options);
+    }
+}
