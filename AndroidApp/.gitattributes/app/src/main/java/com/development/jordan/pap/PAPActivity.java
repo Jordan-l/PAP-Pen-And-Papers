5fb2ef8972af3b3e21d9aa620d463705d782af8c
Jordan-l <jordan-l@freenet.de>
2015-07-12 17:47:28 +0200

    first save naime in variable and some other style fixes

--- AndroidApp/.gitattributes/app/src/main/java/com/development/jordan/pap/PAPActivity.java
+++ AndroidApp/.gitattributes/app/src/main/java/com/development/jordan/pap/PAPActivity.java
@@ -27,8 +27,7 @@ public class PAPActivity extends Activity {
 
     View.OnClickListener myhandler1 = new View.OnClickListener() {
         public void onClick(View v) {
-            // Intent in = new Intent(PAPActivity.this, PlayerStats.class);
-           // startActivity(in);
+
 
             startActivity(new Intent(PAPActivity.this,SaveGameAS.class));
         }
--- AndroidApp/.gitattributes/app/src/main/java/com/development/jordan/pap/SaveGameAS.java
+++ AndroidApp/.gitattributes/app/src/main/java/com/development/jordan/pap/SaveGameAS.java
@@ -6,11 +6,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.view.View.OnClickListener;
+import android.widget.EditText;
 
 public class SaveGameAS extends Activity {
+
+    //Variables
+    CharSequence gameName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.save_game_as);
+
+        Button saveGame = (Button)findViewById(R.id.BSaveGamel);
+        saveGame.setOnClickListener(myhandler1);
     }
+
+    View.OnClickListener myhandler1 = new View.OnClickListener() {
+        public void onClick(View v) {
+            EditText text = (EditText)findViewById(R.id.EDGameName);
+            gameName = text.getText().toString();
+            startActivity(new Intent(SaveGameAS.this,PlayerStats.class));
+        }
+    };
 }
